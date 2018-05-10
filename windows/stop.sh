ca69df942ec1e830cbde620b26945aef98da0917
scadapy <32699436+scadapy@users.noreply.github.com>
2018-05-10 22:13:11 +0300

    Delete stop.sh

--- windows/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'Stop python scripts'
-kill -9 `cat F:\scadapy\339\windows\modbus.pid`
-kill -9 `cat F:\scadapy\339\windows\mclient.pid`
-kill -9 `cat F:\scadapy\339\windows\jserver.pid`
-kill -9 `cat F:\scadapy\339\windows\dbserver.pid`
